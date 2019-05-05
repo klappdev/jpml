b34a4d37eeda224657524e7e8a9c33a9125af65e
klappdev <koowaah@gmail.com>
2019-05-05 11:34:50 +0300

    Initial support tuple pattern

--- src/org/kl/handle/TuplePattern.java
+++ src/org/kl/handle/TuplePattern.java
@@ -1,5 +1,58 @@
 package org.kl.handle;
 
+import org.kl.error.PatternException;
+import org.kl.reflect.Reflection;
+
 public class TuplePattern {
-    /* TODO: release in future */
+
+    public static <V1, V2> void matches(V1 firstValue, V2 secondValue,
+                                        V1 firstData,  V2 secondData, Runnable firstBranch) throws PatternException {
+        if (!Reflection.checkTypes(firstValue.getClass(),  firstData.getClass()) ||
+            !Reflection.checkTypes(secondValue.getClass(), secondData.getClass())) {
+            throw new PatternException("Types in brunches must to be equals");
+        }
+
+        if (firstValue.equals(firstData) && secondValue.equals(secondData)) {
+            firstBranch.run();
+        }
+    }
+
+    public static <V1, V2> void matches(V1 firstValue, V2 secondValue,
+                                        V1 firstData,  V2 secondData, Runnable firstBranch,
+                                        V1 thirdData,  V2 fourthData, Runnable secondBranch) throws PatternException {
+        if (!Reflection.checkTypes(firstValue.getClass(),  firstData.getClass())  ||
+            !Reflection.checkTypes(secondValue.getClass(), secondData.getClass()) ||
+            !Reflection.checkTypes(firstValue.getClass(),  thirdData.getClass())  ||
+            !Reflection.checkTypes(secondValue.getClass(), fourthData.getClass())) {
+            throw new PatternException("Types in brunches must to be equals");
+        }
+
+        if (firstValue.equals(firstData) && secondValue.equals(secondData)) {
+            firstBranch.run();
+        } else if (firstValue.equals(thirdData) && secondValue.equals(fourthData)) {
+            secondBranch.run();
+        }
+    }
+
+    public static <V1, V2> void matches(V1 firstValue, V2 secondValue,
+                                        V1 firstData,  V2 secondData, Runnable firstBranch,
+                                        V1 thirdData,  V2 fourthData, Runnable secondBranch,
+                                        V1 fifthData,  V2 sixthData,  Runnable thirdBranch) throws PatternException {
+        if (!Reflection.checkTypes(firstValue.getClass(),  firstData.getClass())  ||
+            !Reflection.checkTypes(secondValue.getClass(), secondData.getClass()) ||
+            !Reflection.checkTypes(firstValue.getClass(),  thirdData.getClass())  ||
+            !Reflection.checkTypes(secondValue.getClass(), fourthData.getClass()) ||
+            !Reflection.checkTypes(firstValue.getClass(),  fifthData.getClass())  ||
+            !Reflection.checkTypes(secondValue.getClass(), sixthData.getClass())) {
+            throw new PatternException("Types in brunches must to be equals");
+        }
+
+        if (firstValue.equals(firstData) && secondValue.equals(secondData)) {
+            firstBranch.run();
+        } else if (firstValue.equals(thirdData) && secondValue.equals(fourthData)) {
+            secondBranch.run();
+        } else if (firstValue.equals(fifthData) && secondValue.equals(sixthData)) {
+            thirdBranch.run();
+        }
+    }
 }
--- test/org/kl/TuplePatternTest.java
+++ test/org/kl/TuplePatternTest.java
@@ -1,5 +1,39 @@
 package org.kl;
 
+import org.junit.jupiter.api.Test;
+import org.kl.error.PatternException;
+
+import static org.kl.handle.TuplePattern.matches;
+
 public class TuplePatternTest {
-    /* TODO: release in future */
+
+    @Test
+    public void matchesStatementTest() throws PatternException {
+        /* 1 */
+        byte width1 = 10;
+        String side1 = "top";
+
+        matches(width1, side1,
+                (byte) 10, "top", () -> System.out.println("side[" + side1 + "]=" + width1)
+        );
+
+        /* 2 */
+        short width2 = 15;
+        String side2 = "bottom";
+
+        matches(width2, side2,
+                (short) 10, "top",    () -> System.out.println("side[" + side2 + "]=" + width2),
+                (short) 15, "bottom", () -> System.out.println("side[" + side2 + "]=" + width2)
+        );
+
+        /* 2 */
+        int width3 = 20;
+        String side3 = "left";
+
+        matches(width3, side3,
+                10, "top",    () -> System.out.println("side[" + side3 + "]=" + width3),
+                15, "bottom", () -> System.out.println("side[" + side3 + "]=" + width3),
+                20, "left",   () -> System.out.println("side[" + side3 + "]=" + width3)
+        );
+    }
 }
